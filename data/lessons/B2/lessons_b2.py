c443dc90da8b423806738f4abaed936fbb3514fd
ybousquet <ybousquet@onestock-retail.com>
2023-04-24 14:05:40 +0200

    add A2, B1, B2 and C2 lessons

---
+++ data/lessons/B2/lessons_b2.py
@@ -0,0 +1,52 @@
+lesson_B2_could_have_would_have_should_have_presentation_cards = {
+    "deck": "english::lessons::B2::could_have_would_have_should_have::presentation",
+    "basic_cards": [
+        ("What is a modal?", "A modal is an auxiliary verb and normally needs another verb to function"),
+        ("What is the function of a modal?", "The function of a modal is to modify a verb with extra meaning"),
+        ("How use present modal and past modal?", "modal for present modal and modal + have for past modal."),
+        ("How contract the past modal?", "Could've, Should've, Would've"),
+        
+        ("Which modal is use for recommendation/advise?", "Should (If it rains, you should take the umbrella.)"),
+        ("Which modal is use for possibilities?", "Could (If you have time, maybe we could go for a walk?)"),
+        ("Which modal is use for imagining results?", "Would (If I were rich, I would buy a boat.)"),
+        
+        ("Which modal + have say that a different action was recommended in the past?", "Should have (I should have left my house earlier. There’s so much traffic.)"),
+        ("Which modal + have is use to tell other people that a different action in the past would have been better?", " Should have (You shouldn’t have watched YouTube all weekend.)"),
+        
+        ("Which modal + have is use to talk about possibilities if something had been diferent in the past?", "Could have (If I had gone to university, I could have gotten a better job.)"),
+        ("Which modal + have is use to express an amaginary past situation?", "Could have (We could have joined the circus if we were more flexible.)"),
+        
+        ("Which modal + have is use to imagine a result (if something had been different in the past)?", "Would have (If we had arrived earlier, we would have caught the bus.)"),
+        ("Which modal + have is use to epress more certainty about the result than 'could have'?", "Would have (If I had worked harder, I would have gotten a promotion)"),
+        ("", ""),
+    ],
+    "reversed_cards": [
+    ],
+}
+lesson_B2_could_have_would_have_should_have_exercise_cards = {
+    "deck": "english::lessons::B2::could_have_would_have_should_have::exercise",
+    "basic_cards": [
+        ("John didn't finish the task. He _ have done it yesterday - it was quite urgent.", "should"),
+        ("If I had arrived at the clinic earlier, I _ have seen a doctor.", "could"),
+        ("I _ have revised more for my exams. I think I'll fail!", "should"),
+        ("If I had won the lottery this weekend, I _ have booked a trip around the world straightaway.", "would"),
+        ("I didn't know you were going to Phil's party yesterday. You _ have told me!", "should"),
+        ("I can't believe Jim hasn't arrived yet. He _ have taken the wrong train again.", "could"),
+        ("Don't lie to me. You _ not have been ill - Lucca said you were at the ice hockey match last night.", "could"),
+        ("She _ have flown to Prague as it is quicker, but she is scared of flying, so she took the train instead.", "should"),
+        ("If you had studied Chinese, you _ have worked at the Chinese embassy.", "could"),
+        ("You _ have worn sunscreen - your entire back is sunburnt!", "should"),
+        ("If you had left the house just five minutes earlier, you _ have arrived on time.", "would"),
+        ("I _ not have eaten that extra piece of cake - I feel sick now!", "should"),
+        ("If we had taken the faster train, the price of the ticket _ have been significantly higher.", "would"),
+        ("Thanks for all your support during the marathon! I _ not have done it without you guys.", "could"),
+        ("The concert is sold out now. I _ have bought tickets earlier!", "should"),
+        ("We _ have gone on holiday to the Caribbean, but then we got a big tax bill that we had to pay.", "could"),
+        ("If he had worked just a little bit harder, he _ have received a promotion.", "would"),
+        ("She _ have checked the document before she sent it - it was full of mistakes.", "should"),
+        (" If I had worked harder in Chemistry, I _ have been a scientist.", "could"),
+    ],
+    "reversed_cards": [
+
+    ],
+}
